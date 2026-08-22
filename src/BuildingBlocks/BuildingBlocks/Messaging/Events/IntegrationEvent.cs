02b454c679bc5be671c7c44efb89a271bebeda4b
Aboubakr Jelloulat <abubakerjelloulat@gmail.com>
2026-08-22 09:55:13 +0100

    Add message broker event contracts
    Adds integration event contracts for basket checkout and a shared base event model, plus a MassTransit extension for wiring the message broker. Updates Basket and Ordering API appsettings with RabbitMQ connection configuration.

---
+++ src/BuildingBlocks/BuildingBlocks/Messaging/Events/IntegrationEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlock.Messaging.Events;
+
+public class IntegrationEvent
+{
+    public Guid Id => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName!;
+
+}
---
+++ src/BuildingBlocks/BuildingBlocks/Messaging/MassTransit/Extentions.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using MassTransit.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlock.Messaging.MassTransit;
+
+public static class Extentions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly is not null) // Find all classes in this assembly that implement IConsumer<T> and register them with MassTransit
+                config.AddConsumers(assembly);
+
+            config.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(configuration["MessageBroker:UserName"]!);
+                    host.Password(configuration["MessageBroker:Password"]!);
+                });
+
+                configurator.ConfigureEndpoints(context); // Automatically creates queues for each consumer + Binds them to the correct exchanges
+            });
+
+        });
+
+
+        return (services);
+    }
+
+}
